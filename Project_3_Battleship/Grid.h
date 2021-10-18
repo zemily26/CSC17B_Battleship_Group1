d0a37ba6cf36c154b1cf9add5924edb9b105217c
lm2203793 <79282205+lm2203793@users.noreply.github.com>
2021-10-17 22:28:24 -0700

    Update Grid.h
    Clean up and add destructor

--- Project_3_Battleship/Grid.h
+++ Project_3_Battleship/Grid.h
@@ -5,18 +5,14 @@
 #include "Cell.h"
 
 class Grid {
-public:
-    Grid();
-    Grid(const Grid& orig);
-    virtual ~Grid();
-    
-    // X , Y Format
-    Cell& GetCell(int col,int row);
-    
-    void Destroy();
 private:
     Cell** board;
     void CreateBoard();
+public:
+    Grid();
+    ~Grid();
+    Cell& GetCell(int col,int row); // X , Y Format
+    void Destroy(); 
 };
 
 #endif /* GRID_H */
